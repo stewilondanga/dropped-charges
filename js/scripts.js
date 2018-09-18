6b5523e42f836c2597980be7fe9c0d39c513835f
Stewart ilondanga <stilondanga@gmail.com>
2018-09-18 17:32:19 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,30 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+document.addEventListener("DOMContentLoaded", function(){
+	let faces = document.querySelectorAll(".face"),
+		radios = document.querySelectorAll("input"),
+		faceCN = "face",
+		defaultEm = "normal";
+
+	// emotion changes
+	for (var r in radios) {
+		if (+r < radios.length) {
+            radios[r].addEventListener("change", function(){
+				if (this.name == "top") {
+					faces[0].className = faceCN;
+					if (this.value != defaultEm) {
+					    faces[0].classList.add(this.value);
+					}
+
+				} else {
+					faces[1].className = faceCN;
+					if (this.value != defaultEm) {
+					    faces[1].classList.add(this.value);
+					}
+				}
+			});
+		}
+	};
+});
