8bea9e099412ad0fbd9db8be4e3a1f545f0ca93e
Stewart ilondanga <stilondanga@gmail.com>
2018-09-18 18:47:03 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -8,28 +8,28 @@ var navigate = (function() {
 })();
 
 document.addEventListener("DOMContentLoaded", function() {
-      let faces = document.querySelectorAll(".face"),
-        radios = document.querySelectorAll("input"),
-        faceCN = "face",
-        defaultEm = "normal";
-      /*
-      	// emotion changes
-      	for (var r in radios) {
-      		if (+r < radios.length) {
-                  radios[r].addEventListener("change", function(){
-      				if (this.name == "top") {
-      					faces[0].className = faceCN;
-      					if (this.value != defaultEm) {
-      					    faces[0].classList.add(this.value);
-      					}
+  let faces = document.querySelectorAll(".face"),
+    radios = document.querySelectorAll("input"),
+    faceCN = "face",
+    defaultEm = "normal";
 
-      				} else {
-      					faces[1].className = faceCN;
-      					if (this.value != defaultEm) {
-      					    faces[1].classList.add(this.value);
-      					}
-      				}
-      			});
-      		}
-      	};
+  // emotion changes
+  for (var r in radios) {
+    if (+r < radios.length) {
+      radios[r].addEventListener("change", function() {
+        if (this.name == "top") {
+          faces[0].className = faceCN;
+          if (this.value != defaultEm) {
+            faces[0].classList.add(this.value);
+          }
+
+        } else {
+          faces[1].className = faceCN;
+          if (this.value != defaultEm) {
+            faces[1].classList.add(this.value);
+          }
+        }
       });
+    }
+  };
+});
